215ae6621c3b628554db06fd4457e27b5b7ef0d3
yurisilva2503 <yurigsfer@gmail.com>
2024-04-20 10:13:58 -0300

    update for new names of columns in database

--- db-postgres.js
+++ db-postgres.js
@@ -6,7 +6,7 @@ export class databasePG{
         let videos = ''
 
         if(search){
-            videos = await sql`select * from videos where titulo ilike ${search}`
+            videos = await sql`select * from videos where title ilike ${search}`
         }else{
             videos = await sql`select * from videos`
         }
@@ -16,18 +16,18 @@ export class databasePG{
     async create(video){
         const { title, description, duration } = video
         const id = randomUUID()
-        const insert_video = await sql`insert into videos (video_id, titulo, descricao, duracao) values (${id}, ${title}, ${description}, ${duration})`
+        const insert_video = await sql`insert into videos (id, title, description, duration) values (${id}, ${title}, ${description}, ${duration})`
         return insert_video
     }
 
     async update(id, video){
         const { title, description, duration } = video
-        const update_video = sql`update videos set titulo = ${title}, descricao = ${description}, duracao = ${duration} where video_id = ${id}`
+        const update_video = sql`update videos set title = ${title}, description = ${description}, duration = ${duration} where id = ${id}`
         return update_video
     }
     
     async delete(id){
-        const delete_video = await sql`delete from videos where video_id = ${id}`
+        const delete_video = await sql`delete from videos where id = ${id}`
         return delete_video 
     }
 }
